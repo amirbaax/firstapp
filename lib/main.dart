322693514a73a39a274202d2d21df564aa01bde1
amirbaax <amir.bax93@gmail.com>
2022-06-30 18:11:51 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+//void main() {
+//  runApp(MyApp());
+//}
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('answer chosen!');
+  }
+
+  get raisedButtonStyle => null;
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
